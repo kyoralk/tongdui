7e8f6f999a5f7b71a569eddbec82dde100623171
kyoralk <327993561@qq.com>
2017-03-08 18:12:33 +0800

    layer update

---
+++ Application/Mobile/Model/PickInfoModel.class.php
@@ -0,0 +1,27 @@
+<?php
+namespace Mobile\Model;
+use Think\Model\RelationModel;
+
+class PickInfoModel extends RelationModel{
+	protected $_link = array(
+			'order_goods' =>array(
+					'mapping_type'=>self::HAS_MANY,
+					'class_name'=>'order_goods',
+					'foreign_key'=>'order_sn',
+					'mapping_name'=>'order_goods',
+			),
+			'store' =>array(
+					'mapping_type'=>self::BELONGS_TO,
+					'class_name'=>'store',
+					'foreign_key'=>'store_id',
+					'mapping_name'=>'store',
+					'mapping_fields'=>'store_id,store_name',
+			),
+            'deliver'=>array(
+                'mapping_type'=>self::HAS_ONE,
+                'class_name'=>'deliver',
+                'foreign_key'=>'order_sn',
+                'mapping_name'=>'deliver',
+            ),
+	);
+}
